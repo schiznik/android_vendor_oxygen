f1a91d991acce12db2335c84c3a3680c7ed651c0
Adam Green <greena88@gmail.com>
2011-09-11 12:30:34 +0100

    Cleanup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ add_lunch_combo oxygen_bravo-user
 add_lunch_combo oxygen_bravoc-user
 add_lunch_combo oxygen_crespo-user
 add_lunch_combo oxygen_crespo4g-user
-add_lunch_combo oxygen_galaxys2-eng
+add_lunch_combo oxygen_galaxys2-user
 add_lunch_combo oxygen_passion-user
