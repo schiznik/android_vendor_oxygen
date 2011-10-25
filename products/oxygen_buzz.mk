27f437c3be86fd4f930a24e0ef0064873cfde245
schiznik <git@schiznik.com>
2011-10-25 01:55:50 +0100

    Re-Sync with Oxygen's Bravo.mk

--- products/oxygen_buzz.mk
+++ products/oxygen_buzz.mk
@@ -7,7 +7,7 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := buzz
 PRODUCT_MODEL := HTC Wildfire
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=buzz BUILD_ID=GRK39F BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=buzz BUILD_ID=GRK39F BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 
 PRODUCT_PACKAGES += \
     FM \
@@ -23,8 +23,6 @@ PRODUCT_LOCALES += ldpi mdpi
 # Extra overlay for LDPI
 PRODUCT_PACKAGE_OVERLAYS += vendor/oxygen/overlay/ldpi
 
-ifdef OXYGEN_RELEASE
-    PRODUCT_PROPERTY_OVERRIDES += ro.modversion=Oxygen-2.3.1-Buzz
-else
-    PRODUCT_PROPERTY_OVERRIDES += ro.modversion=Oxygen-2-$(shell date +%d%m%Y)-NIGHTLY-Buzz
-endif
+PRODUCT_RELEASE_NAME := Buzz
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/oxygen/products/common_versions.mk
