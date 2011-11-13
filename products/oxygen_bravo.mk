cb9652f9e81dddbf97d3c3bfd38e55edc20b1ec3
Adam Green <greena88@gmail.com>
2011-11-13 18:03:17 +0000

    Removing support for FM.

--- products/oxygen_bravo.mk
+++ products/oxygen_bravo.mk
@@ -9,16 +9,6 @@ PRODUCT_MODEL := HTC Desire
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=bravo BUILD_ID=GRK39F BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 
-PRODUCT_PACKAGES += \
-    FM \
-    hcitool \
-    Torch
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.enable.managed.roaming=1 \
-    ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
-    ro.ril.emc.mode=2
-
 PRODUCT_COPY_FILES += \
     vendor/oxygen/prebuilt/etc/init.d/10apps2sd:system/etc/init.d/10apps2sd
 
