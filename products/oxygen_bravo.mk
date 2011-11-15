2e5f075e54b3e1b90d69bb1cdec1b6b16535832e
schiznik <git@schiznik.com>
2011-11-15 14:24:09 +0000

    keep Torch on bravo & buzz

--- products/oxygen_bravo.mk
+++ products/oxygen_bravo.mk
@@ -9,6 +9,8 @@ PRODUCT_MODEL := HTC Desire
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=bravo BUILD_ID=GRK39F BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 
+PRODUCT_PACKAGES += Torch
+
 PRODUCT_COPY_FILES += \
     vendor/oxygen/prebuilt/etc/init.d/10apps2sd:system/etc/init.d/10apps2sd
 
