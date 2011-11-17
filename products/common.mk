932f71ec7f697333e21918c4f60001a13ff9cac8
schiznik <git@schiznik.com>
2011-11-17 10:50:47 +0000

    Me != AdamG

--- products/common.mk
+++ products/common.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, vendor/google/gapps.mk)
 
-PRODUCT_PROPERTY_OVERRIDES += ro.rommanager.developerid=adamg
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifneq ($(BOARD_KERNEL_MODULES_DIR),)
